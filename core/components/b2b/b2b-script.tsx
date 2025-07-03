8b0fa35b7096eae9af1f215eba47c07131f6a55a
Travis Poole <travis.poole@bigcommerce.com>
2025-07-03 09:08:32 -0500

    Add B2B script debugging and environment variable fixes

--- core/components/b2b/b2b-script.tsx
+++ core/components/b2b/b2b-script.tsx
@@ -1,10 +1,21 @@
 import Script from 'next/script';
 
 export function B2BScript() {
+  const storeHash = process.env.BIGCOMMERCE_STORE_HASH;
+  const channelId = process.env.BIGCOMMERCE_CHANNEL_ID;
+
+  console.log('B2B Script Debug:', {
+    storeHash,
+    channelId,
+    b2bApiHost: process.env.B2B_API_HOST,
+    b2bApiToken: process.env.B2B_API_TOKEN ? 'SET' : 'NOT SET'
+  });
+
   return (
     <>
-      <Script>
+      <Script id="b2b-config" strategy="beforeInteractive">
         {`
+        console.log('B2B Script Loading...');
         window.b3CheckoutConfig = {
           routes: {
             dashboard: '/#/dashboard',
@@ -12,26 +23,30 @@ export function B2BScript() {
         }
         window.B3 = {
           setting: {
-            store_hash: '${process.env.BIGCOMMERCE_STORE_HASH}',  
-            channel_id: ${process.env.BIGCOMMERCE_CHANNEL_ID},
+            store_hash: '${storeHash}',  
+            channel_id: ${channelId},
           },
         }
+        console.log('B2B Config:', window.B3);
         `}
       </Script>
       <Script
         type="module"
         crossOrigin=""
         src="https://buyer-portal.bigcommerce.com/index.*.js"
+        strategy="afterInteractive"
       />
       <Script
         noModule
         crossOrigin=""
         src="https://buyer-portal.bigcommerce.com/polyfills-legacy.*.js"
+        strategy="afterInteractive"
       />
       <Script
         noModule
         crossOrigin=""
         src="https://buyer-portal.bigcommerce.com/index-legacy.*.js"
+        strategy="afterInteractive"
       />
     </>
   );
